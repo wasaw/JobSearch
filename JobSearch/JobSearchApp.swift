efb76c521fff1c786d3edef4f855ada4ed00ba2d
wasaw <merenkovalexander@gmail.com>
2024-03-13 10:59:17 +0300

    Init

---
+++ JobSearch/JobSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSearchApp.swift
+//  JobSearch
+//
+//  Created by Александр Меренков on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct JobSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
