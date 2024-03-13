05a7adaabef2c7e4fb93c45f310f875192b08fe4
wasaw <merenkovalexander@gmail.com>
2024-03-13 16:02:32 +0300

    Search bar UI

--- JobSearch/Model.swift
+++ JobSearch/Model.swift
@@ -8,5 +8,7 @@
 import SwiftUI
 
 struct CustomColor {
+    static let gray2 = Color("Gray2")
     static let gray3 = Color("Gray3")
+    static let gray4 = Color("Gray4")
 }
--- JobSearch/View/Search.swift
+++ JobSearch/View/Search.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct Search: View {
     var body: some View {
-        VStack {
-            Text("Search")
-                .foregroundStyle(Color.white)
-            Divider()
-            Spacer()
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    Spacer()
+                        .frame(width: 12)
+                    HStack {
+                        Image("Search")
+                            .renderingMode(.template)
+                            .foregroundStyle(CustomColor.gray4)
+                        Text("Должность, ключевые слова")
+                            .font(.system(size: 14))
+                            .foregroundStyle(CustomColor.gray4)
+                        Spacer()
+                    }
+                    .padding(11)
+                    .background(CustomColor.gray2)
+                    .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                    Button(action: {
+                        
+                    }, label: {
+                        Image("Slider")
+                            .renderingMode(.template)
+                            .foregroundStyle(.white)
+                    })
+                    .frame(width: 40, height: 40)
+                    .background(CustomColor.gray2)
+                    .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 12))
+                Divider()
+                Spacer()
+            }
         }
-        .background(Color.black)
     }
 }
 
